67c0b5b20d9c9cdcb04b7fa9d7079f3f3628c751
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-10-19 14:01:11 -0700

    Working with asp.net example and will alter it for part 3. DOing this assignment a bit backwords for the ability to have a walkthrough of MVC

--- weeks/week_4/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ weeks/week_4/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -15,10 +16,23 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
             return View();
         }
-
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
