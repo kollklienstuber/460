2a1af8eb8fb30d1f7d1f5e0808206d7e3ee73d5c
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-10-25 14:28:16 -0700

    parts one two and three done, but could add some more styling to them.

--- weeks/week_4/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ weeks/week_4/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -49,10 +49,55 @@ namespace PartyInvites.Controllers
         {
             int age = Int32.Parse(Request.Form["age"]);
             string name = Request.Form["name"];
-            int curLifeSpan = 79;
-            int lifeLeft = curLifeSpan - age;
-            return Content($"{name} if you are currently {age} years old and plan to live to be the age of an average american then you have " + lifeLeft + " years left to live!");
+            decimal curLifeSpan = 79;
+            decimal percentLeft = age / curLifeSpan;
+            decimal lifeLeft = curLifeSpan - age;
+            decimal b = Decimal.Parse(percentLeft.ToString("#.00"));
+
+            return Content($"{name} if you are currently {age} years old and plan to" +
+                $" live to be the age of an average" +
+                $"american (79 years old) then you have {lifeLeft} years left to live! To put it" +
+                $" in a different perspective you have lived {b} percent of your life.");
 
         }
+
+        [HttpGet]
+        public ActionResult Macros()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Macros(FormCollection form)
+        {
+            string name = Request.Form["name"];
+            int age = Int32.Parse(Request.Form["age"]);
+            int weight = Int32.Parse(Request.Form["weight"]);
+
+            double protien;
+            double fat;
+
+
+            string gender = Request.Form["gender"];
+
+            if (gender == "Female" || gender == "f" || gender == "F" ||gender ==  "female") {
+                protien = weight / 1.7;
+                protien = Math.Round(protien, 2);
+
+                fat = weight * 0.35;
+                ViewBag.caloriesToEat = ($"{name}Based on your information you should eat {protien} grams of protein, {fat} grams of fat, and fill the rest with carbs or eat kit kats. I'm not a doctor");
+            }
+
+            else
+            protien = weight / 1.4;
+            protien = Math.Round(protien, 2);
+
+            fat = weight * 0.4;
+            ViewBag.caloriesToEat = ($"{name} Based on your information you should eat {protien} grams of protein, {fat} grams of fat, and fill the rest with carbs or eat kit kats. I'm not a doctor");
+
+
+            return View();
+        }
+        
     }
 }
--- weeks/week_4/PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ weeks/week_4/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -10,14 +10,15 @@ namespace PartyInvites.Models
     {
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Please enter your email address")]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Will you attend?")]
+        public bool? WillAttend { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
-        [Required(ErrorMessage = "Please specify whether you'll attend")]
-        public bool? WillAttend { get; set; }
-
+        [Required(ErrorMessage = "Please enter your e-mail address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
 
     }
+
+
 }
