e9c753a949a75d2a7439d161e43028e9d6090afb
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-11-06 21:31:04 -0800

    controller fixed

---
+++ weeks/week_5/week5c/week5c/Controllers/FormsController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using week5c.Models;
+using System;
+using System.Web.Mvc;
+using week5c.DAL;
+using System.Net;
+using System.Data.Entity;
+
+
+namespace week5c.Controllers
+{
+
+
+    public class FormsController : Controller
+    {
+
+
+        private DAL.FormContext db = new DAL.FormContext();
+
+
+
+        // GET: Forms
+        public ActionResult Index()
+        {
+            return View(db.Forms.ToList());
+        }
+
+        // GET: Forms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Forms/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID, FirstName, LastName, DOB, City, NewAddress, NewState, Zip, County")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forms.Add(form);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(form);
+        }
+
+        // POST: Forms/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,DOB")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(form).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(form);
+        }
+
+
+    }
+}
