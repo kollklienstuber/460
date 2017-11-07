4a1feb2b6a37a5bf1c955ce7cd01e4d165461962
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-11-06 21:34:43 -0800

    connection string and DAL

---
+++ weeks/week_5/week5c/week5c/DAL/FormContext.cs
@@ -0,0 +1,17 @@
+﻿using week5c.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace week5c.DAL
+{
+    public class FormContext : DbContext
+    {
+        public FormContext() : base("name=OurDBContext")
+        { }
+
+        public virtual DbSet<Form> Forms { get; set; }
+    }
+}
