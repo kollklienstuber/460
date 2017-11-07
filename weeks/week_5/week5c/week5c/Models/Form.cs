5c6e60a43fcc3c636e8f573439b2767bac3d799e
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-11-06 21:28:32 -0800

    model

---
+++ weeks/week_5/week5c/week5c/Models/Form.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace week5c.Models
+{
+    public class Form
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required, StringLength(128)]
+        [Display(Name = "Address")]
+        public string NewAddress { get; set; }
+
+
+
+        [Display(Name = "DOB")]
+        public DateTime DOB { get; set; }
+
+        [Required, StringLength(128)]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+
+        [Display(Name = "County")]
+        public string County { get; set; }
+
+
+        [Required, StringLength(64)]
+        [Display(Name = "State")]
+        public string NewState { get; set; }
+
+
+        [Display(Name = "Zip")]
+        public int Zip { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {FirstName} {LastName} DOB = {DOB} {NewAddress} {City} {NewState} Zip = {Zip}";
+        }
+    }
+}
