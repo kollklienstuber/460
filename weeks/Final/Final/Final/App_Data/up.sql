fb6072bf1132536b9fa67f5ff586dbb185d82821
kollklienstuber <kklienstuber15@mail.wou.edu>
2017-12-05 15:03:33 -0800

    1st commit, finalfix is correct version, final gave an error that Ive never seen when populated data that had to do with maximum attempts allowed so it seemed safer to just create new

---
+++ weeks/Final/Final/Final/App_Data/up.sql
@@ -0,0 +1,87 @@
+﻿-- Users Buyer
+CREATE TABLE dbo.Buyers
+(	
+	--Buyer PK as ID
+	BuyerID	INT IDENTITY (1,1) NOT NULL,
+	--Buyer name
+	BuyerName	NVARCHAR(64) NOT NULL,
+	
+	-- constraints for a PK
+	CONSTRAINT [PK_dbo.Buyers] PRIMARY KEY CLUSTERED (BuyerID ASC)
+);
+
+CREATE TABLE dbo.Sellers
+(
+	SellerID	INT IDENTITY (1,1) NOT NULL,
+	SellerName NVARCHAR(64) NOT NULL,
+	
+	-- constraints for a PK
+	CONSTRAINT [PK_dbo.Sellers] PRIMARY KEY CLUSTERED (SellerID ASC)
+);
+
+CREATE TABLE dbo.Items
+(
+	--sense Genres is a single column table we dont need to link it to anything VIA a FK 
+	--Genre is linked to others because others (classifications) needs to know about it, 
+	--but it doesnt need to know about anyone else
+	ItemID			INT IDENTITY (1,1) NOT NULL,
+	ItemName		NVARCHAR(64) NOT NULL,
+	ItemDescription	NVARCHAR(128) NOT NULL,
+	SellerID INT,
+
+
+	CONSTRAINT [PK_dbo.Items] PRIMARY KEY CLUSTERED (ItemID ASC),
+
+	-- add in seller as a FK
+	CONSTRAINT FK_SellerID FOREIGN KEY (SellerID)
+	REFERENCES Sellers(SellerID),
+
+);
+
+
+CREATE TABLE dbo.Bidss
+(
+BidID INT IDENTITY (1,1) NOT NULL,
+BuyerID INT,
+ItemID	INT,
+BidPrice DECIMAL NOT NULL,
+TimeOfBid datetime NOT NULL,
+CONSTRAINT[PK_dbo.Bid] PRIMARY KEY CLUSTERED (BidID ASC),
+CONSTRAINT FK_ItemID FOREIGN KEY (ItemID)
+REFERENCES Item(ItemID),
+CONSTRAINT FK_BuyerID FOREIGN KEY (BuyerID)
+REFERENCES Buyer(BuyerID)
+);
+
+
+
+--Once tables are created insert into them the appropriate data
+
+--Insert into artists table
+INSERT INTO dbo.Buyers (BuyerName) VALUES 
+	('Jane Stone'),
+	('Tom McMasters'),
+	('Otto Vanderwall');
+GO
+
+--Insert into ArtWorks table
+INSERT INTO dbo.Sellers (SellerName) VALUES 
+('Gayle Hardy'),
+('Lyle Banks'),
+('Pearl Greene');
+GO
+
+--Insert into Classifications table
+INSERT INTO dbo.Items(ItemName, ItemDescription) VALUES 
+('Abraham Lincoln Hammer'    ,'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln'),
+('Albert Einsteins Telescope','A brass telescope owned by Albert Einstein in Germany, circa 1927'),
+('Bob Dylan Love Poems'      ,'Five versions of an original unpublished, handwritten, love poem by Bob Dylan');
+GO
+
+--Insert into Classifications table
+INSERT INTO dbo.Bids(ItemID, BidPrice, TimeOfBid) VALUES 
+(1001,250000,'2017-12-04 09:04:22'),
+(1003,95000 ,'2017-12-04 08:44:03');
+GO
+
+
---
+++ weeks/FinalFix/FinalFix/App_Data/up.sql
@@ -0,0 +1,76 @@
+﻿CREATE TABLE dbo.Buyers
+(
+BuyerID INT IDENTITY (1,1) NOT NULL,
+BuyerName	NVARCHAR(128) NOT NULL,
+
+CONSTRAINT[PK_dbo.Buyer] PRIMARY KEY CLUSTERED (BuyerID ASC)
+);
+
+
+CREATE TABLE dbo.Sellers
+(
+SellerID INT IDENTITY (1,1) NOT NULL,
+SellerName	NVARCHAR(128) NOT NULL,
+
+CONSTRAINT[PK_dbo.Seller] PRIMARY KEY CLUSTERED (SellerID ASC)
+);
+
+
+CREATE TABLE dbo.Items
+(
+ItemID INT IDENTITY (1,1) NOT NULL,
+ItemName	NVARCHAR(128) NOT NULL,
+ItemDescription NVARCHAR (128) NOT NULL,
+
+SellerID INT,
+CONSTRAINT[PK_dbo.Item] PRIMARY KEY CLUSTERED (ItemID ASC),
+CONSTRAINT FK_SellerID FOREIGN KEY (SellerID)
+REFERENCES Sellers(SellerID)
+);
+
+
+CREATE TABLE dbo.Bids
+(
+BidID INT IDENTITY (1,1) NOT NULL,
+ItemID	INT,
+BuyerID INT,
+TimeOfBid datetime NOT NULL,
+PriceOfItem DECIMAL NOT NULL,
+
+CONSTRAINT[PK_dbo.Bid] PRIMARY KEY CLUSTERED (BidID ASC),
+
+
+CONSTRAINT FK_ItemID FOREIGN KEY (ItemID)
+REFERENCES Items(ItemID),
+
+
+CONSTRAINT FK_BuyerID FOREIGN KEY (BuyerID)
+REFERENCES Buyers(BuyerID)
+);
+
+
+INSERT INTO dbo.Buyers (BuyerName) VALUES 
+	('Jane Stone'),
+	('Tom McMasters'),
+	('Otto Vanderwall');
+GO
+
+
+INSERT INTO dbo.Sellers (SellerName) VALUES 
+	('Gayle Hardy'),
+	('Lyle Banks'),
+	('Pearl Greene');
+GO
+
+
+INSERT INTO dbo.Items (ItemName, ItemDescription, SellerID) VALUES 
+	('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln', '3'),
+	('Albert Eisteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927', '1'),
+	('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan', '2');
+GO
+
+
+INSERT INTO dbo.Bids(ItemID, BuyerID, PriceOfItem, TimeOfBid) VALUES 
+	('1','3', '250000', '2017-12-04 09:04:22'),
+	('2','1', '95000','2017-12-04 08:44:03');
+GO
